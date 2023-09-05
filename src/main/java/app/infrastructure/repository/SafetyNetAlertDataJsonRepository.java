7d51e1edf502e463538cc66cd67ce9bb243a5068
ZinebELHALLAOUI <zinebhellaoui@gmail.com>
2023-09-06 01:11:05 +0200

    Create SafetyNetAlertDataJsonRepository

---
+++ src/main/java/app/infrastructure/repository/SafetyNetAlertDataJsonRepository.java
@@ -0,0 +1,28 @@
+package app.infrastructure.repository;
+
+import app.infrastructure.entity.SafetyNetAlertData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SafetyNetAlertDataJsonRepository {
+    protected final SafetyNetAlertData safetyNetAlertData;
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+    private File jsonFilePath = new File("src/main/resources/safetyNetAlertData.json");
+
+    public SafetyNetAlertDataJsonRepository() {
+        SafetyNetAlertData safetyNetAlertData;
+        try {
+            safetyNetAlertData = objectMapper.readValue(jsonFilePath, SafetyNetAlertData.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read file : " + jsonFilePath, e);
+        }
+        this.safetyNetAlertData = safetyNetAlertData;
+    }
+
+    protected SafetyNetAlertDataJsonRepository(final File jsonFilePath) {
+        this();
+        this.jsonFilePath = jsonFilePath;
+    }
+}
